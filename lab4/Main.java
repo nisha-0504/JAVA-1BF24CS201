aec9bf1127139f28fa24508a20db8785f3f099e0
Nisha Kumari <nishakumari.cs24@bmsce.ac.in>
2025-12-13 13:45:01 +0530

    Create Main.java

---
+++ lab4/Main.java
@@ -0,0 +1,41 @@
+abstract class Shape{
+    int a,b;
+    abstract void printArea();
+}
+class Rectangle extends Shape{
+    Rectangle(int length, int breadth){
+        a=length;
+        b=breadth;
+    }
+    void printArea(){
+        System.out.println("Area of rectangle: "+(a*b));
+    }
+}
+class Triangle extends Shape{
+    Triangle(int base, int height){
+        a=base;
+        b=height;
+    }
+    void printArea(){
+        System.out.println("Area of Triangle: "+(0.5*a*b));
+    }
+}
+class Circle extends Shape{
+    Circle(int radius){
+        a=radius;
+    }
+    void printArea(){
+        System.out.println("Area of circle: "+(3.14*a*a));
+    }
+}
+public class Main
+{
+    public static void main(String[] args) {
+        Rectangle r=new Rectangle(10,5);
+        Triangle t=new Triangle(8,6);
+        Circle c=new Circle(7);
+        r.printArea();
+        t.printArea();
+        c.printArea();
+    }
+} 
